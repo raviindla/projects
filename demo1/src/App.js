efaae6396dec1e4285b0bbce9cae3f154896e360
raviindla <105688735+raviindla@users.noreply.github.com>
2022-06-08 21:25:11 -0400

    Add files via upload

---
+++ demo1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CI CD React Setup demo</h1>
+      <h2>Added code</h2>
+    </div>
+  );
+}
+
+export default App;
